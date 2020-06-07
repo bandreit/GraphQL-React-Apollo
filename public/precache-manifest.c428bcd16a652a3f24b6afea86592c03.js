682fa61983ec15638d56655155c3bc09adac6ccb
Andrei Bostan <abostan.ir@gmail.com>
2020-06-07 16:40:43 +0200

    ready to deploy

---
+++ public/precache-manifest.c428bcd16a652a3f24b6afea86592c03.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48c2d3ca5f6038ff0fcd9d065ef6b0b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3aa795b742e8eb1bc79",
+    "url": "/static/css/2.34ca2c66.chunk.css"
+  },
+  {
+    "revision": "99a84210bdc28337c558",
+    "url": "/static/css/main.19fdfa1c.chunk.css"
+  },
+  {
+    "revision": "c3aa795b742e8eb1bc79",
+    "url": "/static/js/2.84b76dd0.chunk.js"
+  },
+  {
+    "revision": "f0d43bb23094b147d2c500466b241ef2",
+    "url": "/static/js/2.84b76dd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99a84210bdc28337c558",
+    "url": "/static/js/main.4bc99834.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "4243060d7b24a2bc4f57ce8348a5b6c5",
+    "url": "/static/media/logo.4243060d.jpg"
+  }
+]);
